4e2c8ad89da34206a1a519db21394fb74ec3801b
ycambro <yurgenmora03@gmail.com>
2024-09-23 16:20:22 -0600

    Cambios menores a las migraciones

--- BookShoppingCartMvcUI/Migrations/20240917011903_init.Designer.cs
+++ BookShoppingCartMvcUI/Migrations/20240917011903_init.Designer.cs
@@ -12,15 +12,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookShoppingCartMvcUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240426055110_mig-stock-mgt")]
-    partial class migstockmgt
+    [Migration("20240917011903_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.3")
+                .HasAnnotation("ProductVersion", "8.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
