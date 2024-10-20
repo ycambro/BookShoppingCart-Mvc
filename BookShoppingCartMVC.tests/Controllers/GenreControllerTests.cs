2204e14a0767d84da833c3bf364e4d133afe7ec7
Yosward García <yosward.gar@gmail.com>
2024-10-20 13:04:46 -0600

    Prueba de Genre controller completas

--- BookShoppingCartMVC.tests/Controllers/GenreControllerTests.cs
+++ BookShoppingCartMVC.tests/Controllers/GenreControllerTests.cs
@@ -146,5 +146,128 @@ namespace BookShoppingCartMVC.tests.Controllers
             Assert.Equal(nameof(_controller.AddGenre), redirectResult.ActionName);  // Verificar redirección
             _mockRepo.Verify(repo => repo.AddGenre(It.IsAny<Genre>()), Times.Once);  // Verificar que se intentó agregar el género
         }
+
+
+        [Fact]
+        public async Task UpdateGenre_GetGenre_ReturnsViewWithGenre()
+        {
+            // Arrange
+            var genre = new Genre { Id = 1, GenreName = "Action" };
+            _mockRepo.Setup(repo => repo.GetGenreById(1)).ReturnsAsync(genre);
+
+            // Act
+            var result = await _controller.UpdateGenre(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<GenreDTO>(viewResult.Model);
+
+            Assert.Equal(genre.Id, model.Id);
+            Assert.Equal(genre.GenreName, model.GenreName);
+
+        }
+
+        [Fact]
+        public async Task UpdateGenre_GenreNotFound_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetGenreById(1)).ReturnsAsync((Genre)null);
+
+            // Act
+            var result = _controller.UpdateGenre(1);
+
+            // Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await result);
+        }
+
+        [Fact]
+        public async Task UpdateGenre_ValidModel_UpdateGenreAndRedirects()
+        {
+            var genreDTO = new GenreDTO { Id = 1, GenreName = "Mistery" };
+            _mockRepo.Setup(repo => repo.UpdateGenre(It.IsAny<Genre>())).Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.UpdateGenre(genreDTO);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+            _mockRepo.Verify(repo => repo.UpdateGenre(It.IsAny<Genre>()), Times.Once);
+            Assert.Equal("Genre is updated successfully", _controller.TempData["successMessage"]);
+        }
+
+        [Fact]
+        public async Task UpdateGenre_InvalidModelWithNoGenreName_ReturnsViewWithModel()
+        {
+            // Arrange
+            var genreDTO = new GenreDTO { Id = 2, GenreName = "" }; // Nombre del género vacío
+            _controller.ModelState.AddModelError("GenreName", "Required");
+
+            // Act
+            var result = await _controller.UpdateGenre(genreDTO);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<GenreDTO>(viewResult.Model);
+            Assert.Equal(2, model.Id);
+            Assert.Equal("", model.GenreName);
+        }
+
+
+        [Fact]
+        public async Task UpdateGenre_ExceptionDuringUpdate_ShowsErrorMessageAndReturnModel()
+        {
+            // Arrange
+            var genreDTO = new GenreDTO { Id = 1, GenreName = "Drama"};
+            _mockRepo.Setup(repo => repo.UpdateGenre(It.IsAny<Genre>())).ThrowsAsync(new Exception("Error"));
+
+            // Act
+            var result = await _controller.UpdateGenre(genreDTO);
+
+            // Assert
+            var viewResulta= Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<GenreDTO>(viewResulta.Model);
+            Assert.Equal(1, model.Id);
+            Assert.Equal("Drama", model.GenreName);
+            Assert.Equal("Genre could not updated!", _controller.TempData["errorMessage"]);
+        }
+
+        [Fact]
+        public async Task UpdateGenre_ValidModel_CallsRepositoryOnce()
+        {
+            // Arrange
+            var genreDTO = new GenreDTO { Id = 1, GenreName = "Drama" };
+            _mockRepo.Setup(repo => repo.UpdateGenre(It.IsAny<Genre>())).Returns(Task.CompletedTask);
+
+            // Act
+            await _controller.UpdateGenre(genreDTO);
+
+            // Assert
+            _mockRepo.Verify(repo => repo.UpdateGenre(It.IsAny<Genre>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Index_ReturnsViewWithGenresList()
+        {
+            // Arrange
+            var genresList = new List<Genre>
+                {
+                    new Genre { Id = 1, GenreName = "Horror" },
+                    new Genre { Id = 2, GenreName = "Science Fiction" }
+                };
+
+            _mockRepo.Setup(repo => repo.GetGenres()).ReturnsAsync(genresList);
+
+            // Act
+            var result = await _controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Genre>>(viewResult.Model);
+            Assert.Equal(2, model.Count());  // Verifica que hay dos géneros en la lista.
+            Assert.Equal("Horror", model.First().GenreName);  // Verifica que el primer género es "Horror".
+        }
+
+
     }
 }
