86abfd8a829789b10683f4b25ad16bc35d74141b
Yosward García <yosward.gar@gmail.com>
2024-11-10 15:25:51 -0600

    Pruebas automatizadas para Book

---
+++ BookShoppingAutomatedTest/BookShoppingAutomatedTest/Pages/BookPage.cs
@@ -0,0 +1,271 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoppingAutomatedTest.Pages
+{
+    public class BookPage : Base
+    {
+        By DashboardOptionLocator = By.XPath("//*[@id=\"navbarColor01\"]/ul[1]/li[3]/a");
+        By BooksOptionLocator = By.XPath("/html/body/div/div[1]/a[5]");
+
+        By AddMoreBtnLocator = By.XPath("/html/body/div/div[2]/div/a");
+        By BookNameInputLocator = By.Id("BookName");
+        By AuthorInputLocator = By.Id("AuthorName");
+
+        By GenreDropdownListLocator = By.Id("GenreId");
+        By PriceInputLocator = By.Id("Price");
+
+        By AddBtnLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[6]/button");
+
+        By BooknameFieldRequiredMsgLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[1]/span");
+        By AuthornameFieldRequiredMsgLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[2]/span");
+        By GenreFieldRequiredMsgLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[3]/span");
+
+        By PriceFieldRequiredMsgLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[4]/span");
+
+        By ResultsLocator = By.TagName("tr");
+
+        By BackBtnLocator = By.XPath("/html/body/div/div[2]/div/div/a");
+
+        By EditFirstBtnLocator = By.XPath("/html/body/div/div[2]/div/table/tbody/tr[2]/td[6]/a[1]");
+        By UpdateBtnLocator = By.XPath("/html/body/div/div[2]/div/div/form/div[6]/button");
+        By FirtsTableRowLocator = By.XPath("/html/body/div/div[2]/div/table/tbody/tr[2]");
+
+        By FirstDeleteBtnLocator = By.XPath("/html/body/div/div[2]/div/table/tbody/tr[2]/td[6]/a[2]");
+
+        public BookPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void EnsureLoggedIn(string username, string password)
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login(username, password);
+        }
+
+
+        public void ManageBooks()
+        {
+            Click(DashboardOptionLocator);
+            Click(BooksOptionLocator);
+
+        }
+
+        public void AddBook(String bookName, String author, String genre, String price  )
+        {
+            Click(AddMoreBtnLocator);
+            Type(bookName, BookNameInputLocator);
+            Type(author, AuthorInputLocator);
+
+            IWebElement genreDropdown = FindElement(GenreDropdownListLocator);
+            List<IWebElement> genres = genreDropdown.FindElements(By.TagName("option")).ToList();
+
+            for(int i = 0; i < genres.Count; i++)
+            {
+                if (genres[i].Text == genre)
+                {
+                    genres[i].Click();
+                    break;
+                }
+            }
+
+            Clear(PriceInputLocator);
+
+            Type(price, PriceInputLocator);
+            Click(AddBtnLocator);
+        }
+
+        public void EditFirstBook(String newPrice, String newBookName, String author, String genre)
+        {
+            Click(EditFirstBtnLocator);
+
+            if(newPrice != "")
+            {
+                Clear(PriceInputLocator);
+                Type(newPrice, PriceInputLocator);
+            }
+            if(newBookName != "")
+            {
+                Clear(BookNameInputLocator);
+                Type(newBookName, BookNameInputLocator);
+            }
+            if (author != "")
+            {
+                Clear(AuthorInputLocator);
+                Type(author, AuthorInputLocator);
+            }
+            if (genre != "")
+            {
+                IWebElement genreDropdown = FindElement(GenreDropdownListLocator);
+                List<IWebElement> genres = genreDropdown.FindElements(By.TagName("option")).ToList();
+
+                for (int i = 0; i < genres.Count; i++)
+                {
+                    if (genres[i].Text == genre)
+                    {
+                        genres[i].Click();
+                        break;
+                    }
+                }
+            }
+
+            Click(UpdateBtnLocator);
+
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        public void DeleteFirstBook()
+        {
+            Click(FirstDeleteBtnLocator);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => IsAlertPresent());
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+
+        }
+
+        public int CountBook()
+        {
+            int numberOfRows = FindElements(ResultsLocator).Count;
+            return numberOfRows - 1; // -1 because the first row is the header
+        }
+
+        public Boolean IsFirstPriceUpdated(String newPrice)
+        {
+            IWebElement firstRow = FindElement(FirtsTableRowLocator);
+            List<IWebElement> cells = firstRow.FindElements(By.TagName("td")).ToList();
+
+            return GetText(cells[4]) == newPrice;
+        }
+
+
+        public Boolean IsFormEmpty()
+        {
+            Boolean BookNameEmpty = GetText(BookNameInputLocator) == "";
+            Boolean AuthorEmpty = GetText(AuthorInputLocator) == "";
+            Boolean PriceEmpty = GetText(PriceInputLocator) == "";
+            Console.Write(BookNameEmpty);
+            Console.Write(AuthorEmpty);
+            Console.Write(PriceEmpty);
+            return BookNameEmpty && AuthorEmpty && PriceEmpty;
+        }
+
+        public Boolean IsBookNameFieldRequiredMsgDisplayed()
+        {
+            return IsDisplayed(BooknameFieldRequiredMsgLocator);
+        }
+
+        public Boolean IsGenreFieldRequiredMsgDisplayed()
+        {
+            return IsDisplayed(GenreFieldRequiredMsgLocator);
+        }
+
+        public Boolean IsAuthorFieldRequiredMsgDisplayed()
+        {
+            return IsDisplayed(AuthornameFieldRequiredMsgLocator);
+        }
+
+        public Boolean IsPriceFieldRequiredMsgDisplayed()
+        {
+            return IsDisplayed(PriceFieldRequiredMsgLocator);
+        }
+
+        public Boolean IsAllMsgDisplayed()
+        {
+            return IsBookNameFieldRequiredMsgDisplayed() 
+                && IsAuthorFieldRequiredMsgDisplayed() 
+                && IsGenreFieldRequiredMsgDisplayed() 
+                && IsPriceFieldRequiredMsgDisplayed();
+        }
+
+        public Boolean IsNegativePriceMsgDisplayed(String book)
+        {
+            Click(BackBtnLocator);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => FindElements(ResultsLocator).Count > 0);
+            List<IWebElement> rows = FindElements(ResultsLocator);
+            for (int i = 1; i < rows.Count; i++)
+            {
+                List<IWebElement> cells = rows[i].FindElements(By.TagName("td")).ToList();
+
+
+                if (cells.Count > 0)
+                {
+                    string bookName = cells[1].Text;
+                    string priceText = cells[4].Text;
+
+                    if (bookName.Equals(book, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (decimal.TryParse(priceText, out decimal price))
+                        {
+                            if (price < 0)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Boolean IsBookRepeated(String book)
+        {
+            Click(BackBtnLocator);
+            List<IWebElement> rows = FindElements(ResultsLocator);
+            int counter = 0;
+            for (int i = 1; i < rows.Count; i++)
+            {
+                List<IWebElement> cells = rows[i].FindElements(By.TagName("td")).ToList();
+                if (cells.Count > 0)
+                {
+                    string bookName = cells[1].Text;
+
+                    if (bookName.Equals(book, StringComparison.OrdinalIgnoreCase))
+                    {
+                        counter++;
+
+                        if (counter > 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public Boolean IsFirstBookAuthorUpdated(String author)
+        {
+            IWebElement firstRow = FindElement(FirtsTableRowLocator);
+            List<IWebElement> cells = firstRow.FindElements(By.TagName("td")).ToList();
+            return GetText(cells[2]) == author;
+        }
+
+        public Boolean IsFirstBookNameUpdated(String book)
+        {
+            IWebElement firstRow = FindElement(FirtsTableRowLocator);
+            List<IWebElement> cells = firstRow.FindElements(By.TagName("td")).ToList();
+            return GetText(cells[1]) == book;
+        }
+
+    }
+}
---
+++ BookShoppingAutomatedTest/BookShoppingAutomatedTest/Tests/BookTests.cs
@@ -0,0 +1,153 @@
+﻿using BookShoppingAutomatedTest.Pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoppingAutomatedTest.Tests
+{
+
+    
+    public class BookTests
+    {
+
+        private IWebDriver driver;
+        BookPage bookPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bookPage = new BookPage(driver);
+            driver = bookPage.ChromeDriverConnection();
+            driver.Manage().Window.Maximize();
+            bookPage.Visit("http://localhost:5232/");
+
+            bookPage.EnsureLoggedIn("admin@gmail.com", "Admin@123");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        [Test]
+        public void RegisterBookWithValidData_IsRegistered()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("Twilight", "Stephenie Meyer", "Romance", "233");
+            Assert.IsTrue(bookPage.IsFormEmpty());
+        }
+
+        [Test]
+        public void RegisterBookWithEmptyForm_DisplayAllMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("", "", "", "");
+            Assert.IsTrue(bookPage.IsAllMsgDisplayed());
+        }
+
+
+        [Test]
+        public void RegisterBookWithNoGenre_DisplayGenreMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("The little prince", "Antoine de Saint", "", "123");
+            Assert.IsTrue(bookPage.IsGenreFieldRequiredMsgDisplayed());
+        }
+
+        [Test]
+        public void RegisterBookWithoutBookname_DisplayBooknameMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("", "Isabel Chaves", "Programming", "345");
+            Assert.IsTrue(bookPage.IsBookNameFieldRequiredMsgDisplayed());
+        }
+
+        [Test]
+        public void RegisterBookWithoutAuthor_DisplayAuthorMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("Diary of a Wimpy Kid", "", "Thriller", "110");
+            Assert.IsTrue(bookPage.IsAuthorFieldRequiredMsgDisplayed());
+        }
+
+        [Test]
+        public void RegisterBookWithoutPrice_DisplayPriceMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("The Hill", "Isaac Mora", "Thriller", "");
+            Assert.IsTrue(bookPage.IsPriceFieldRequiredMsgDisplayed());
+        }
+
+        [Test]
+        public void RegisterBookWithNegativePrice_DisplayPriceMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("Angels and Demons", "Francisco Paredes", "Thriller", "-235");
+            Assert.IsTrue(bookPage.IsNegativePriceMsgDisplayed("Angels and Demons"));
+        }
+
+        [Test]
+        public void RegisterBookWithExistingBook_DisplayBookExistsMsg()
+        {
+            bookPage.ManageBooks();
+            bookPage.AddBook("Pride and Prejudice", "Jane Austen", "Romance", "13");
+            Assert.IsFalse(bookPage.IsBookRepeated("Pride and Prejudice"));
+        }
+
+        [Test]
+        public void UpdatePriceWithIntegerValueFirstBook_DisplayBookWithnewPrice()
+        {
+            bookPage.ManageBooks();
+            bookPage.EditFirstBook("13", "", "", "");
+            Thread.Sleep(2000);
+            Assert.IsTrue(bookPage.IsFirstPriceUpdated("13"));
+        }
+
+
+        [Test]
+        public void UpdatePriceWithFloatValueFirstBook_DisplayBookWithnewPrice()
+        {
+            bookPage.ManageBooks();
+            bookPage.EditFirstBook("10,99", "", "", "");
+            Thread.Sleep(2000);
+            Assert.IsTrue(bookPage.IsFirstPriceUpdated("10,99"));
+        }
+
+
+        [Test] 
+        public void UpdateAuthor_DisplayBookWithNewAuthor()
+        {
+            bookPage.ManageBooks();
+            bookPage.EditFirstBook("", "", "Andrea Perez", "");
+            Thread.Sleep(2000);
+            Assert.IsTrue(bookPage.IsFirstBookAuthorUpdated("Andrea Perez"));
+        }
+
+        [Test]
+        public void UpdateBokkName_DisplayBookWithNewName()
+        {
+            bookPage.ManageBooks();
+            bookPage.EditFirstBook("", "Harry potter", "", "");
+            Thread.Sleep(2000);
+            Assert.IsTrue(bookPage.IsFirstBookNameUpdated("Harry potter"));
+        }
+
+
+        [Test]
+        public void DeleteBook_DeletedBookIsNotDisplayed()
+        {
+            bookPage.ManageBooks();
+            int booksBefore = bookPage.CountBook();
+            bookPage.DeleteFirstBook();
+            Thread.Sleep(2000);
+            int booksAfter = bookPage.CountBook();
+            Assert.IsTrue(booksBefore > booksAfter);
+        }
+
+    }
+}
